f7d68ccc2f801bf2e850bc4f47650579b0c07bf7
MARCOS PEDRO ALVES <marcos.palves4@senacsp.edu.br>
2022-05-06 19:39:16 -0300

    Final da aula

--- projeto-investimentos/interfaces/crud.interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface Crud
-{
-  public function criar(array $dados): array;
-  public function editar(array $dados): array;
-  public function listar(): array;
-}
--- projeto-investimentos/telas/footer.frag.php
+++
@@ -1,6 +0,0 @@
-<footer>
-  <span class="d-block text-center">Isso é um footer</span>
-</footer>
-</body>
-
-</html>
